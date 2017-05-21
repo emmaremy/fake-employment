c68a5ce9f238c97098d13cbe10ed8fd8eaaf0149
emmaremy <eremy1@swarthmore.edu>
2017-05-21 12:30:37 -0400

    switching things up?

---
+++ map.js
@@ -0,0 +1,20 @@
+var svg = d3.select("body")
+    .append("svg");
+
+var path = d3.geoPath();
+
+d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+    if (error) throw error;
+
+    svg.append("g")
+        .attr("class", "counties")
+      .selectAll("path")
+      .data(topojson.feature(us, us.object.counties).features)
+      .enter().append("path")
+        .attr("d", path);
+
+    svg.append("path")
+        .attr("class", "borders")
+        .attr("d", path(topojson.mesh(us.us.objects.states, function(a, b) P
+                    return a !== b; })));
+        });
