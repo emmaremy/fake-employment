fa5fbbd7ff9d6c65eeabdbce1d458f0ca1439491
emmaremy <eremy1@swarthmore.edu>
2017-05-21 12:32:13 -0400

    silly typo

--- map.js
+++ map.js
@@ -15,6 +15,6 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
 
     svg.append("path")
         .attr("class", "borders")
-        .attr("d", path(topojson.mesh(us.us.objects.states, function(a, b) P
-                    return a !== b; })));
-        });
+        .attr("d", path(topojson.mesh(us.us.objects.states, function(a, b) {
+            return a !== b; })));
+});
