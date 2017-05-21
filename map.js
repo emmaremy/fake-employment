1e1e6ea016cdc3e58b30106453e4a9ea0e3e1642
emmaremy <eremy1@swarthmore.edu>
2017-05-21 12:33:21 -0400

    another typo

--- map.js
+++ map.js
@@ -9,12 +9,12 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
     svg.append("g")
         .attr("class", "counties")
       .selectAll("path")
-      .data(topojson.feature(us, us.object.counties).features)
+      .data(topojson.feature(us, us.objects.counties).features)
       .enter().append("path")
         .attr("d", path);
 
     svg.append("path")
         .attr("class", "borders")
-        .attr("d", path(topojson.mesh(us.us.objects.states, function(a, b) {
+        .attr("d", path(topojson.mesh(us.us.objects.counties, function(a, b) {
             return a !== b; })));
 });
